62246714ffe88738016fed6e8a5aac44df435a44
inayat-amin <84003766+inayat-amin@users.noreply.github.com>
2023-01-15 22:33:12 -0500

    Create 12.sql

---
+++ 12.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name LIKE "%johnny%depp%" AND movies.title IN(SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE people.name LIKE "%Helena%Bonham%Carter%");
